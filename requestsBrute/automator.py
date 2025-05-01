d850b682b975616d347339b4b0294d23fb1dc934
deemon... <97549749+BOI1000@users.noreply.github.com>
2025-05-01 12:21:04 -0600

    Update automator.py
    Fixed quotes

--- requestsBrute/automator.py
+++ requestsBrute/automator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import os
-os.system('python3 requestsBrute.py http://example.com/login' 'wordlist.txt')
+os.system('python3 requestsBrute.py http://example.com/login wordlist.txt')
 # This script is a simple automator for the requestsBrute.py script.
 # I am NOT using bash for this repo. Keeping it pythonic.
 # Of course, edit the URL and wordlist to your liking.
